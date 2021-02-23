b61034c788db535af0da1aa73fc32aba62ca199e
kharanga <60679652+kharanga@users.noreply.github.com>
2021-02-22 21:40:58 -0800

    Delete StudentTest.java

--- StudentTest.java
+++
@@ -1,9 +0,0 @@
-package assignment2;
-
-public class StudentTest {
-	public static void main(String[]args) {
-		Student student = new Student("John", "Smith", 3.6, 20, "Computer Science", "School of Computer Science departmen" );
-		Student.Course course = student.new Course();
-		course.printSchedule();
-	}
-}
